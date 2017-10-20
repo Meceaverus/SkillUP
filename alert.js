6bf79046bb7203cb89c2b20e2744358ef3d41e3a
Meceaverus <Meceaverus22@gmail.com>
2017-10-20 21:04:07 +0300

    20/10/2017

---
+++ alert.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var hello = 'Hello world!';
+var message;
+
+// скопировали значение
+message = hello;
+
+alert( hello ); // Hello world!
+alert( message ); // Hello world!
+
+
+    var admin, name;
+
+    name = 'Уарабэй';
+
+    admin = name;
+
+    alert ( admin );
+
+    var
