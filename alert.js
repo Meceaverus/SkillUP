ddfb3c6eabe9c3927b4d6602115534d0c6c964a8
Meceaverus <Meceaverus22@gmail.com>
2017-10-21 13:10:13 +0300

    Homework5_21.10.2017

---
+++ alert.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var c = "15";
+var d = "2";
+var result = 17
+
+alert( result ); // Задача 2
+
+var a = "10";
+var b = "2";
+var c = "5";
+
+alert( +a + +b + +c ); // Задача 3
+
+var a = 17;
+var b = 10;
+var c = a - b;
+var d = c
+
+alert ( c = d ); // Задача 4
+
+var c = 7;
+var c = d;
+var result = 14;
+
+alert ( result ); // Задача 5
+
+var str = "Привет Глеб :)";
+str = 'Привет Глеб :)';
+alert( str ); // Задача 6
+
+var str1 = 'Привет,';
+var str2 = 'Богдан';
+alert ( str1 + str2 ); // Задача 7
+
+var str = 'Привет,';
+var admin, name;
+name = 'Богдан';
+admin = name;
+alert ( str + admin ); // Задача 8
