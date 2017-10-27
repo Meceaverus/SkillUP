fb71895c7157257dbe705224dab0830df25c538d
Meceaverus <32491542+Meceaverus@users.noreply.github.com>
2017-10-27 19:45:30 +0300

    Update massives.js

--- massives.js
+++ massives.js
@@ -1,15 +1,42 @@
 'use script';
 
-//Задача 1
+var arr = [],
+    x = '',
+    i;
 
-//Задача 2
+for (i = 0; i <10; i++) {
+    x += 'x';
+    arr.push(x);
+}
 
-//Задача 3
+console.log(arr); // Задача 1
 
-//Задача 4
+function arrayFill(s, count) {
+    var i,
+        res = '';
 
-//Задача 5
+    for (i = 0; i < count; i++){
+        res += s;
+    }
 
-//Задача 6
+    return res;
+}
 
-//Задача 7
+console.log(arrayFill('123-', 10)); //Задача 3
+
+function arrayFill(s, count) {
+    var i,
+        res = '';
+
+    for (i = 0; i < count; i++) {
+        res += s;
+    }
+    return(res);
+}
+var i,
+    arr = [];
+
+for (i=1; i <= 9; i++){
+    arr.push(arrayFill(i, i));
+}
+    console.log(arr); //Задача 2
